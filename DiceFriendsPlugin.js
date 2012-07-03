85bf147fc639c2db8c9f5aad90f727dc01cae2b9
azixMcAze <mathieu.leber@gmail.com>
2012-07-03 21:00:51 +0200

    Added licence and author information

--- DiceFriendsPlugin.js
+++ DiceFriendsPlugin.js
@@ -1,24 +1,39 @@
 /*
-var mydiv = $('#mydiv');
-if(mydiv.length > 0)
-{
-    mydiv.remove();
-}
-mydiv = $('<div id="mydiv"/>'),
-$('#content .base-middle-content').prepend(mydiv);
+Copyright (c) 2012, Mathieu Le Ber
+All rights reserved.
 
-function print(msg)
-{
-    $('#mydiv').append('<p>' + msg + '</p>');
-}
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met: 
 
-function printObj(o)
-{
-	for(var k in o)
-	{
-		print(k + ' : ' + o[k]);
-	}
-}
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer. 
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution. 
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+The views and conclusions contained in the software and documentation are those
+of the authors and should not be interpreted as representing official policies, 
+either expressed or implied, of the FreeBSD Project.
+*/
+
+/*
+* DiceFriends, a plugin for Better Battlelog that adds the playing Dice employees to your comcenter.
+*
+* @author Mathieu Le Ber
+* @version 1.0
+* @date 30.06.2012
+* @url https://github.com/azixMcAze/DiceFriends
 */
 
 DiceFriendsPlugin = {
