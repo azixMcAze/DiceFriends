d283621bd003e2c15fba71ad441a4dfa9f1bab94
azixMcAze <mathieu.leber@gmail.com>
2012-09-25 22:06:44 +0200

    Converted plugin to fetch data from hunt-dice
    - Converted plugin to fetch data from hunt-dice instead of the official
    DICE platoon
    - Added a platform icon
    - Added support for the Dice Friend dogtag

--- DiceFriendsPlugin.js
+++ DiceFriendsPlugin.js
@@ -31,7 +31,7 @@ either expressed or implied, of the FreeBSD Project.
 * DiceFriends, a plugin for Better Battlelog that adds the playing Dice employees to your comcenter.
 *
 * @author azixMcAze
-* @version 1.0.4
+* @version 1.1.0
 * @date 24.09.2012
 * @url https://github.com/azixMcAze/DiceFriends
 *
@@ -41,9 +41,11 @@ either expressed or implied, of the FreeBSD Project.
 DiceFriendsPlugin = {
 	playerList : [],
 	updateInterval : 60*5,
-	dicePlatoon : "/platoon/2832655391300702826/listmembers/",
+	//dicePlatoon : "/platoon/2832655391300702826/listmembers/",
+	huntDiceData : "http://localhost/DiceFriends/data1.js",  //"http://www.furydivine.net/api/hunt-dice/active/",
 	showingDiceFriends : true,
-	
+	platformIcon : { "pc" : "common-game-2-1", "xbox" : "common-game-2-2", "ps3" : "common-game-2-4"},
+
 	init : function()
 	{
 		// add a hook to the refresh function of the comcenter friend list to redraw the dice friend list whenever needed
@@ -87,6 +89,20 @@ DiceFriendsPlugin = {
 				opacity: .4;\n\
 				filter: alpha(opacity=40);\n\
 			}\n\
+			.comcenter-interact-empty\n\
+			{\n\
+				height:24px;\n\
+				width:23px;\n\
+				float:right;\n\
+				margin-top:6px;\n\
+			}\n\
+			.comcenter-username .fake_a\n\
+			{\n\
+				white-space:nowrap;\n\
+				width:132px;\n\
+				text-overflow:ellipsis;\n\
+				overflow:hidden;\n\
+			}\n\
 			'
 		));
 		
@@ -98,12 +114,17 @@ DiceFriendsPlugin = {
 	{
 		this.playerList = [];
 
-		BBLog.getJSONFromBattlelog(this.makeLocalizedUrl(this.dicePlatoon), function(json){
-			DiceFriendsPlugin.parsePlatoonMembers(json);
+		this.getJSON(this.huntDiceData, function(json){
+			DiceFriendsPlugin.parseHuntDiceData(json);
 			DiceFriendsPlugin.displayPlayerList();
 		});
 	},
 	
+	getJSON : function(url, callback)
+	{
+		$.ajax({url:url, dataType:"jsonp", success:callback, jsonpCallback:"callback"});
+	},
+
 	displayComcenterSeparator : function(playerCount)
 	{
 		$('#comcenterDiceFriends').append(
@@ -145,7 +166,7 @@ DiceFriendsPlugin = {
 				$('<div>').addClass('comcenter-avatar').append(
 					$('<div>').attr('rel', player.userId).addClass('base-avatar-container base-avatar-size-small').append(
 						$('<div>').addClass('base-avatar-status-overlay base-avatar-status-overlay-playing').append(
-							$('<img>').attr('src', 'http://www.gravatar.com/avatar/'+ player.userAvatar +'?s=22&d=http%3A%2F%2Fbattlelog-cdn.battlefield.com%2Fpublic%2Fbase%2Fshared%2Fdefault-avatar-22.png%3Fv%3D7909')
+							$('<img>').attr('src', player.userAvatar)
 									  .attr('width', 22)
 									  .attr('height', 22)
 						)
@@ -156,20 +177,34 @@ DiceFriendsPlugin = {
 						.text(player.name)).append(
 					$('<div>').addClass('comcenter-username-serverinfo').append(
 						$('<div>').addClass('base-left').append(
+							$('<span>').addClass('comcenter-full-height common-gameicon-hori bright comcenter-game-icon')
+									   .addClass(this.platformIcon[player.platform])
+						).append(
+							$('<span>').addClass('comcenter-small-height common-gameicon-hori comcenter-game-icon')
+									   .addClass(this.platformIcon[player.platform])
+						)).append(
+						$('<div>').addClass('base-left').append(
 							$('<span>').addClass('common-playing-link').append(
-								$('<a>').attr('title', player.serverName)
-										.addClass('common-playing-link base-no-ajax comcenter-playing-link').attr('href', this.makeLocalizedUrl('/servers/show/'+ player.serverGuid +'/'))
-										.text(player.serverName)
+								player.serverGuid ?
+									($('<a>').attr('title', player.serverName)
+											.addClass('common-playing-link base-no-ajax comcenter-playing-link').attr('href', this.makeLocalizedUrl('/servers/show/'+ player.serverGuid +'/'))
+											.text(player.serverName))
+								:
+									($('<span>').addClass('common-playing-link base-no-ajax comcenter-playing-link fake_a').text(player.serverName))
 							)
 						)
 					)
 				)).append(
 				$('<div>').addClass('comcenter-interact-container').append(
-					$('<form>').addClass('join-friend').attr('method', 'POST').attr('action', this.makeLocalizedUrl('/servers/show/'+ player.serverGuid +'/')).append(
-						$('<input>').attr('type', 'hidden').attr('name', 'game').attr('value', 2)).append(
-						$('<input>').attr('type', 'hidden').attr('name', 'guid').attr('value', player.serverGuid)).append(
-						$('<div>').attr('title', 'Join Game').addClass('bubble-title-left join-friend-submit-link comcenter-interact-playing')
-					)).append(
+					player.serverGuid ?
+						$('<form>').addClass('join-friend').attr('method', 'POST').attr('action', this.makeLocalizedUrl('/servers/show/'+ player.serverGuid +'/')).append(
+							$('<input>').attr('type', 'hidden').attr('name', 'game').attr('value', 2)).append(
+							$('<input>').attr('type', 'hidden').attr('name', 'guid').attr('value', player.serverGuid)).append(
+							$('<div>').attr('title', 'Join Game').addClass('bubble-title-left join-friend-submit-link comcenter-interact-playing')
+						)
+					:
+						$('<div>').addClass('bubble-title-left join-friend-submit-link comcenter-interact-empty'))
+				.append(
 					$('<a>').attr('title', '')
 							.attr('href', this.makeLocalizedUrl('/soldier/' + player.name + '/dogtags/' + player.personaId + '/'))
 							.addClass('bubble-title-left comcenter-interact-dogtag-parent').append(
@@ -236,34 +271,43 @@ DiceFriendsPlugin = {
 	{
 		var dogtagDiv = $('#comcenter-' + player.userId + ' .comcenter-interact-dogtag-parent');
 	
-		if(player.hasDiceDogtag)
+		if(player.hasDiceFriendDogtag)
 		{
-			if(player.hasDevDogtag)
-			{
-				dogtagDiv.attr('title', 'Dice & Dev Dogtags');
-				dogtagDiv.removeClass('no-dice-dogtag');
-				dogtagDiv.removeClass('only-dev-dogtag');
-			}
-			else
-			{
-				dogtagDiv.attr('title', 'Only Dice Dogtag');
-				dogtagDiv.removeClass('no-dice-dogtag');
-				dogtagDiv.removeClass('only-dev-dogtag');
-			}
+			dogtagDiv.attr('title', 'Dice Friend Dogtag');
+			dogtagDiv.removeClass('no-dice-dogtag');
+			dogtagDiv.removeClass('only-dev-dogtag');
 		}
 		else
 		{
-			if(player.hasDevDogtag)
+			if(player.hasDiceDogtag)
 			{
-				dogtagDiv.attr('title', 'Only Dev Dogtag');
-				dogtagDiv.removeClass('no-dice-dogtag');
-				dogtagDiv.addClass('only-dev-dogtag');
+				if(player.hasDevDogtag)
+				{
+					dogtagDiv.attr('title', 'Dice & Dev Dogtags');
+					dogtagDiv.removeClass('no-dice-dogtag');
+					dogtagDiv.removeClass('only-dev-dogtag');
+				}
+				else
+				{
+					dogtagDiv.attr('title', 'Only Dice Dogtag');
+					dogtagDiv.removeClass('no-dice-dogtag');
+					dogtagDiv.removeClass('only-dev-dogtag');
+				}
 			}
 			else
 			{
-				dogtagDiv.attr('title', 'No Dice Dogtags');
-				dogtagDiv.addClass('no-dice-dogtag');
-				dogtagDiv.removeClass('only-dev-dogtag');
+				if(player.hasDevDogtag)
+				{
+					dogtagDiv.attr('title', 'Only Dev Dogtag');
+					dogtagDiv.removeClass('no-dice-dogtag');
+					dogtagDiv.addClass('only-dev-dogtag');
+				}
+				else
+				{
+					dogtagDiv.attr('title', 'No Dice Dogtags');
+					dogtagDiv.addClass('no-dice-dogtag');
+					dogtagDiv.removeClass('only-dev-dogtag');
+				}
 			}
 		}
 	},
@@ -279,52 +323,54 @@ DiceFriendsPlugin = {
 		this.updateServerDisplay(player);
 	},
 */
-	parseUser : function(json, player)
+/*	parseUser : function(json, player)
+  	{
+  		var soldiersBox = json.data.soldiersBox;
+  		for(var i in soldiersBox)
+  		{
+  			var soldier = json.data.soldiersBox[i];
+  			if(soldier.persona.personaId == player.personaId)
+  			{
+  				var dogtags = soldier.dogtagsForPersona[player.personaId];
+  				if(dogtags)
+  				{
+  					player.hasDiceDogtag = (dogtags.basicDogTag.nameSID == "ID_DT_N_DTB090_CAMPAIGN");
+  					player.hasDevDogtag = (dogtags.advancedDogTag.nameSID == "ID_DT_N_DTA_DICE");
+  				}
+  				
+  				this.updateDogtagDisplay(player);
+  				return;
+  			}
+  		}
+  	},
+*/
+	parseHuntDiceData : function(json)
 	{
-		var soldiersBox = json.data.soldiersBox;
-		for(var i in soldiersBox)
-		{
-			var soldier = json.data.soldiersBox[i];
-			if(soldier.persona.personaId == player.personaId)
-			{
-				var dogtags = soldier.dogtagsForPersona[player.personaId];
-				if(dogtags)
-				{
-					player.hasDiceDogtag = (dogtags.basicDogTag.nameSID == "ID_DT_N_DTB090_CAMPAIGN");
-					player.hasDevDogtag = (dogtags.advancedDogTag.nameSID == "ID_DT_N_DTA_DICE");
-				}
-				
-				this.updateDogtagDisplay(player);
-				return;
-			}
-		}
-	},
-
-	parsePlatoonMembers : function(json)
-	{
-		var members = json.context.listMembers;
+		var members = json
 
 		for(var i in members)
 		{
 			var member = members[i];
-			var user = member.user;
+			var user = member.player;
 
 			// if player is member of the platoon (not only invited) and in a PC game
-			if(member.membershipLevel >= json.context.membershipLevels.MEMBER && user.presence.isPlaying && user.presence.platform == platforms.PC )
+			if(user.onlineStatus.isPlaying)
 			{
 				var player =
 				{
-					name : user.username,
-					userId : user.userId,
-					personaId : member.personaId,
-					userAvatar : user.gravatarMd5,
-					serverGuid : user.presence.serverGuid,
-					serverName : user.presence.serverName
+					name : user.profile.playerName,
+					userId : user.profile.playerId,
+					personaId : user.profile.personaId,
+					userAvatar : user.profile.playerImage,
+					serverGuid : user.onlineStatus.serverGUID,
+					serverName : user.onlineStatus.serverName,
+					platform : user.profile.namespace,
+					hasDiceDogtag : user.dogTags.left.nameSID == "ID_DT_N_DTB090_CAMPAIGN",
+					hasDevDogtag : user.dogTags.right.nameSID == "ID_DT_N_DTA_DICE",
+					hasDiceFriendDogtag : user.dogTags.right.nameSID == 'ID_DT_COMMUNITY_N_DOGTAG'
 				}
 				
 				this.playerList.push(player);
-				BBLog.getJSONFromBattlelog(this.makeLocalizedUrl('/user/overviewBoxStats/'+ player.userId +'/'), this.makeCallbackWithParam(this.parseUser, player));
-				//BBLog.getJSONFromBattlelog(this.makeLocalizedUrl('/servers/show/'+ player.serverGuid +'/'), this.makeCallbackWithParam(this.parseServer, player));
 			}
 		}
 	},
