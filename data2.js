d2fbeae869e0b416024db51c95252f10c6c7180f
azixMcAze <mathieu.leber@gmail.com>
2012-09-27 22:32:18 +0200

    removed unused functions

---
+++ data2.js
@@ -0,0 +1,228 @@
+callback([
+    {
+        "player": {
+            "profile": {
+                "playerName": "BFO-Grizzle",
+                "personaId": "331682238",
+                "playerUrl": "http://battlelog.battlefield.com/bf3/user/BFO-Grizzle/",
+                "playerImage": "http://www.gravatar.com/avatar/7a06fd642c7b1fa6f69856f1209d240e?s=30&d=http%3A%2F%2Fbattlelog-cdn.battlefield.com%2Fpublic%2Fbase%2Fshared%2Fdefault-avatar-30.png",
+                "personaName": "BFO-Grizzle",
+                "playerId": "2832658801534078097",
+                "namespace": "pc",
+                "personaUrl": "http://battlelog.battlefield.com/bf3/soldier/BFO-Grizzle/stats/331682238/"
+            },
+            "onlineStatus": {
+                "serverGUID": "b0dff852-e463-40a8-ba44-0821cd12585d",
+                "serverName": "-=NLB=- HARDCORE SQUAD RUSH",
+                "isPlaying": true,
+                "serverLocation": "http://battlelog-cdn.battlefield.com/public/common/flags/gb.gif?v=7310",
+                "serverUrl": "http://battlelog.battlefield.com/bf3/en/servers/show/b0dff852-e463-40a8-ba44-0821cd12585d"
+            },
+            "dogTags": {
+                "specialDogtagEquipped": false,
+                "right": {
+                    "nameSID": "ID_DT_COMMUNITY_N_DOGTAG",
+                    "index": 246,
+                    "image": "communitydogtag",
+                    "isAdvanced": true,
+                    "imageurl": null
+                },
+                "left": {
+                    "nameSID": "ID_DT_XP3_PREM2_N_DTB01_GENERAL",
+                    "index": 179,
+                    "image": "left_premium",
+                    "isAdvanced": null,
+                    "imageurl": null
+                }
+            }
+        }
+    },
+    {
+        "player": {
+            "profile": {
+                "playerName": "FlyingMogge",
+                "personaId": "247402161",
+                "playerUrl": "http://battlelog.battlefield.com/bf3/user/FlyingMogge/",
+                "playerImage": "http://www.gravatar.com/avatar/1c8daf47aeec653d7049c3e99539d0ce?s=30&d=http%3A%2F%2Fbattlelog-cdn.battlefield.com%2Fpublic%2Fbase%2Fshared%2Fdefault-avatar-30.png",
+                "personaName": "FlyingMogge",
+                "playerId": "2832665149461821441",
+                "namespace": "pc",
+                "personaUrl": "http://battlelog.battlefield.com/bf3/soldier/FlyingMogge/stats/247402161/"
+            },
+            "onlineStatus": {
+                "serverGUID": "c75632ff-d0c1-42e9-a03e-9252cc9c8b75",
+                "serverName": "[tSc] The Skulls Club TDM Kharg & Wake",
+                "isPlaying": true,
+                "serverLocation": "http://battlelog-cdn.battlefield.com/public/common/flags/de.gif?v=7310",
+                "serverUrl": "http://battlelog.battlefield.com/bf3/en/servers/show/c75632ff-d0c1-42e9-a03e-9252cc9c8b75"
+            },
+            "dogTags": {
+                "specialDogtagEquipped": false,
+                "right": {
+                    "nameSID": "ID_DT_COMMUNITY_N_DOGTAG",
+                    "index": 246,
+                    "image": "communitydogtag",
+                    "isAdvanced": true,
+                    "imageurl": null
+                },
+                "left": {
+                    "nameSID": "ID_DT_XP3_PREM2_N_DTB002",
+                    "index": 180,
+                    "image": "xp3_prem2_dtb_002",
+                    "isAdvanced": null,
+                    "imageurl": null
+                }
+            }
+        }
+    },
+    {
+        "player": {
+            "profile": {
+                "playerName": "MelTamariz",
+                "personaId": "312323845",
+                "playerUrl": "http://battlelog.battlefield.com/bf3/user/MelTamariz/",
+                "playerImage": "http://www.gravatar.com/avatar/2a03515953a5a646d3463996bf13e29d?s=30&d=http%3A%2F%2Fbattlelog-cdn.battlefield.com%2Fpublic%2Fbase%2Fshared%2Fdefault-avatar-30.png",
+                "personaName": "MelTamariz",
+                "playerId": "2832658801580074257",
+                "namespace": "pc",
+                "personaUrl": "http://battlelog.battlefield.com/bf3/soldier/MelTamariz/stats/312323845/"
+            },
+            "onlineStatus": {
+                "serverGUID": "a19fc58c-6aef-4e39-bc09-be4f9ee76abc",
+                "serverName": "=rTr= DALLAS #2 24/7 SC TDM NOSHAHR- Zylinas Goofy Server",
+                "isPlaying": true,
+                "serverLocation": "http://battlelog-cdn.battlefield.com/public/common/flags/us.gif?v=7310",
+                "serverUrl": "http://battlelog.battlefield.com/bf3/en/servers/show/a19fc58c-6aef-4e39-bc09-be4f9ee76abc"
+            },
+            "dogTags": {
+                "specialDogtagEquipped": false,
+                "right": {
+                    "nameSID": "ID_DT_COMMUNITY_N_DOGTAG",
+                    "index": 246,
+                    "image": "communitydogtag",
+                    "isAdvanced": true,
+                    "imageurl": null
+                },
+                "left": {
+                    "nameSID": "ID_DT_XP3_PREM2_N_DTB005",
+                    "index": 183,
+                    "image": "xp3_prem2_dtb_005",
+                    "isAdvanced": null,
+                    "imageurl": null
+                }
+            }
+        }
+    },
+    {
+        "player": {
+            "profile": {
+                "playerName": "mrFunkyFred",
+                "personaId": "187014176",
+                "playerUrl": "http://battlelog.battlefield.com/bf3/user/mrFunkyFred/",
+                "playerImage": "http://www.gravatar.com/avatar/1588c064d1d07ba9f16429bdd05e2510?s=30&d=http%3A%2F%2Fbattlelog-cdn.battlefield.com%2Fpublic%2Fbase%2Fshared%2Fdefault-avatar-30.png",
+                "personaName": "mrFunkyFred",
+                "playerId": "2832660534564981103",
+                "namespace": "pc",
+                "personaUrl": "http://battlelog.battlefield.com/bf3/soldier/mrFunkyFred/stats/187014176/"
+            },
+            "onlineStatus": {
+                "serverGUID": "92798ac9-b2a7-4b2b-ad5b-0ef6e1d44cb5",
+                "serverName": "Webhallen 10 Bandar Desert 1000 tickets",
+                "isPlaying": true,
+                "serverLocation": "http://battlelog-cdn.battlefield.com/public/common/flags/se.gif?v=7310",
+                "serverUrl": "http://battlelog.battlefield.com/bf3/en/servers/show/92798ac9-b2a7-4b2b-ad5b-0ef6e1d44cb5"
+            },
+            "dogTags": {
+                "specialDogtagEquipped": true,
+                "right": {
+                    "nameSID": "ID_DT_N_DTA_DICE",
+                    "index": 157,
+                    "image": "dice",
+                    "isAdvanced": true,
+                    "imageurl": null
+                },
+                "left": {
+                    "nameSID": "ID_DT_N_DTB090_CAMPAIGN",
+                    "index": 89,
+                    "image": "dtb090",
+                    "isAdvanced": null,
+                    "imageurl": null
+                }
+            }
+        }
+    },
+    {
+        "player": {
+            "profile": {
+                "playerName": "sabz1337",
+                "personaId": "592085551",
+                "playerUrl": "http://battlelog.battlefield.com/bf3/user/sabz1337/",
+                "playerImage": "http://www.gravatar.com/avatar/eb600e1a358ea94365a0f4d2e54f943a?s=30&d=http%3A%2F%2Fbattlelog-cdn.battlefield.com%2Fpublic%2Fbase%2Fshared%2Fdefault-avatar-30.png",
+                "personaName": "sabz1337",
+                "playerId": "2832657337921147507",
+                "namespace": "pc",
+                "personaUrl": "http://battlelog.battlefield.com/bf3/soldier/sabz1337/stats/592085551/"
+            },
+            "onlineStatus": {
+                "serverGUID": "8f858169-8534-45b5-86de-c3c2c9c11e16",
+                "serverName": "xtracommunity.com [64 : Vanilla]",
+                "isPlaying": true,
+                "serverLocation": "http://battlelog-cdn.battlefield.com/public/common/flags/de.gif?v=7310",
+                "serverUrl": "http://battlelog.battlefield.com/bf3/en/servers/show/8f858169-8534-45b5-86de-c3c2c9c11e16"
+            },
+            "dogTags": {
+                "specialDogtagEquipped": true,
+                "right": {
+                    "nameSID": "ID_DT_N_DTA_DICE",
+                    "index": 157,
+                    "image": "dice",
+                    "isAdvanced": true,
+                    "imageurl": null
+                },
+                "left": {
+                    "nameSID": "ID_DT_N_DTB090_CAMPAIGN",
+                    "index": 89,
+                    "image": "dtb090",
+                    "isAdvanced": null,
+                    "imageurl": null
+                }
+            }
+        }
+    },
+    {
+        "player": {
+            "profile": {
+                "playerName": "Eeric85",
+                "personaId": "211145140",
+                "playerUrl": "http://battlelog.battlefield.com/bf3/user/Eeric85/",
+                "playerImage": "http://www.gravatar.com/avatar/09ec788c4af39660a28c693731038868?s=30&d=http%3A%2F%2Fbattlelog-cdn.battlefield.com%2Fpublic%2Fbase%2Fshared%2Fdefault-avatar-30.png",
+                "personaName": "Zombu",
+                "playerId": "2832658801533862164",
+                "namespace": "ps3",
+                "personaUrl": "http://battlelog.battlefield.com/bf3/soldier/Zombu/stats/211145140/ps3/"
+            },
+            "onlineStatus": {
+                "serverName": "-[DICE]- MOST POPULAR - Norm",
+                "isPlaying": true,
+                "serverLocation": null
+            },
+            "dogTags": {
+                "specialDogtagEquipped": false,
+                "right": {
+                    "nameSID": "ID_DT_N_DTA_DICE",
+                    "index": null,
+                    "image": "defaulttag_right",
+                    "isAdvanced": true,
+                    "imageurl": null
+                },
+                "left": {
+                    "nameSID": "ID_DT_N_DTB090_CAMPAIGN",
+                    "index": null,
+                    "image": "defaulttag_left",
+                    "isAdvanced": null,
+                    "imageurl": null
+                }
+            }
+        }
+    }
+])
